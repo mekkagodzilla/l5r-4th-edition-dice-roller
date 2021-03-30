84143e48d540c87d6df6380c156e3e6334e19d03
Philippe Lemaire <lemaire.philippe@gmail.com>
2021-03-30 18:34:25 +0200

    implemented emphasis to reroll 1s.

--- dice-roller.py
+++ dice-roller.py
@@ -6,13 +6,13 @@ for the 4th edition of the Legends of the Five Rings (L5R) RPG
 import random
 
 
-def roll(rolled, keep, unskilled=False):
+def roll(rolled, keep, emphasis=False, keepbest=True, unskilled=False):
 
     '''Rolls 'rolled' d10s, and asks user to keep 'keep' number of dice.
     returns the sum of the value of the dice kept.
     A die resulting in a 10 is exploded.
-    A new d10 is rolled, and its result is added to the die that was a 10.
-    10 dice rule: if rolled > 10, for each 2 extra dice, add a kept dice
+    A new d10 is rolled, and its result is added to the die that was a 10.
+    10 dice rule: if rolled > 10, for each 2 extra dice, add a kept dice
     instead.
     Dice values of 10 are exploded (and extra die is rolled and added to them),
     unless the unskilled parameter is set to True.
@@ -29,6 +29,13 @@ def roll(rolled, keep, unskilled=False):
 
     for die in range(rolled):
         dieFace = random.randint(1, 10)
+        print(f"You rolled a {dieFace}.")
+        # let's reroll ones if we have emphasis
+        if emphasis:
+            if dieFace == 1:
+                print("Let's reroll that nasty 1…")
+                dieFace = random.randint(1, 10) 
+                print(f"You rolled a {dieFace}.")
         # we'll explode the dieFace if we get a 10, and keep going if we get
         # another 10, unless this is an unskilled roll.
         if not unskilled:
@@ -40,21 +47,28 @@ def roll(rolled, keep, unskilled=False):
     
     rawResult.sort(reverse=True)
 
-    print(f'The rawresult is {rawResult}')
+    print(f'The raw result is {rawResult}')
 
     # let's keep some dice
-    keptDice = []
+    if keep >= rolled:
+        print('Keeping all the dice.')
+        keepbest = True
+
+    if keepbest:
+        keptDice = rawResult[0:keep]
+
+    else:
+        keptDice = []
+        for diceToKeep in range(keep):
+            response = 0
+            while int(response) not in rawResult:
+                response = input(f'Please chose a dice to keep among {rawResult}.\n')
+                response = int(response)
+            keptDice.append(response)
+            rawResult.remove(response)
+            print(f'Keeping a {response}…')  
     
-    for diceToKeep in range(keep):
-        response = 0
-        while int(response) not in rawResult:
-            response = input(f'Please chose a dice to keep among {rawResult}.\n')
-            response = int(response)
-        keptDice.append(response)
-        rawResult.remove(response)
-        print(f'Keeping a {response}…')  
-    
-    print(f'You kept {keptDice})
+    print(f'You kept {keptDice}')
     print(f'Your final result is {sum(keptDice)}.')
     return sum(keptDice)
 
